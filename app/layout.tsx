1f61e9987a25ea46a09909277628c3da144fbdab
Javier Ayala <javayala77@gmail.com>
2026-03-30 17:47:57 -0400

    Add Meta Pixel (ID: 1291283423194390) to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,28 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico?v=3" sizes="any" />
         <link rel="icon" href="/logo.png?v=3" type="image/png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/logo.png?v=3" />
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1291283423194390');
+              fbq('track', 'PageView');
+            `
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1291283423194390&ev=PageView&noscript=1"
+          />
+        </noscript>
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17952699649" />
         <script
